7e7019c1ae660dad3ea22db5422bb63a7b87689f
Charles Grant Nichols <grant@inkdots.com>
2021-05-19 13:59:33 -0500

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,53 @@
+#Book Object Factory
+require 'pry'
 
+class Book
+# .new alias
+    def initialize(title)
+        #every instance has this attached to it
+        
+    @title = title
+    end
+#reader
+    def title
+        @title
+    end
+    
+#reader
+    def author
+        @author
+    end 
+
+#writer
+    def author=(author_name)
+        @author = author_name
+
+    end
+
+#reader
+    def page_count
+        @page_count
+    end
+
+#writer
+    def page_count=(page_count)
+         @page_count = page_count
+    end
+
+#reader
+    def genre
+        @genre
+    end
+#writer
+    def genre=(genre)
+        @genre = genre
+    end
+
+
+#instance method
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
 
